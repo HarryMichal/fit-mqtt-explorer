1257eaccd43854768d2cdde27044744d94ce75ce
Ondřej Míchal <harrymichal@seznam.cz>
2021-04-27 00:42:07 +0200

    mqtt: Add proof-of-concept MQTT backend
    This is a very early shot at adding the MQTT client backend. It is only
    capable of connecting to a single server.
    
    All async signals coming from the async MQTT client are handled and
    made available as signals. These can be used by the GUI.
    
    Next steps:
    
    - add a way to disconnect gracefully
    - add options to the connect() method

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,12 @@
 
 #include "mainwindow.h"
 
+Q_DECLARE_METATYPE(mqtt::const_message_ptr)
+
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
+
+    qRegisterMetaType<mqtt::const_message_ptr>("mqtt::const_message_ptr");
 
     app.setApplicationName("fit-mqtt-client");
     app.setApplicationDisplayName("MQTT Client");
