229ab2099967f889aba1e2601e1a256eec8b7c54
Ondřej Míchal <harrymichal@seznam.cz>
2021-04-27 00:45:34 +0200

    gui: Hook up MQTT backend to the GUI
    The toolbar now has a new button: "Connect". Currently, clicking it
    makes the backend connect to a preset server. Ideally, it will spawn
    a dialog window where the user can specify information about the server.
    
    Another new thing is a status bar at the bottom of the window. It seemed
    like an ideal place for showing the state of the connection. Currently
    it shows either "connected" or "disconnected". In the future that could
    be a diode of some sort, name of the server (broker) and more.

--- src/mainwindow.h
+++ src/mainwindow.h
@@ -2,7 +2,11 @@
 #define MAINWINDOW_H
 
 #include <QActionGroup>
+#include <QLabel>
 #include <QMainWindow>
+#include <QThread>
+
+#include "mqttmanager.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -10,6 +14,9 @@ class MainWindow;
 }
 QT_END_NAMESPACE
 
+/**
+ * @brief The ModeState enum represents visible page
+ */
 enum ModeState {
     Dashboard,
     Explorer
@@ -28,11 +35,24 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    QThread worker_thread; //<Thread for running MQTTManager
+    MQTTManager *mqtt_manager; //<Manager of connection to a MQTT broker
 
     ModeState currentMode;
     QActionGroup *modeSelector;
 
+    QLabel connection_status; //<Permanent widget in StatusBar
+
+    void setupStatusBar();
     void setupActions();
+    void updateStatusBar();
+
+private slots:
+    /**
+     * @brief messageReceived is emitted when a message arrives from the server.
+     * @param msg See mqtt::message
+     */
+    void messageReceived(const mqtt::const_message_ptr msg);
 };
 
 #endif // MAINWINDOW_H
