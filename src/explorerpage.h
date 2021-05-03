234ffbc7a280e573630b1a00e00d6b94519336d4
Ondřej Míchal <harrymichal@seznam.cz>
2021-05-03 18:55:35 +0200

    explorerpage: Add means to view message list in a topic
    With this the right part of the Explorer widget finally gets some
    functionality. When a topic is clicked, the message list is populated
    with all messages related to the said topic. When one of the items in
    the list is further clicked on, the message preview is populated with
    the content of the message.
    
    TODO:
    - elipsize messages in the message history list
    - add colors to the messages

--- src/explorerpage.h
+++ src/explorerpage.h
@@ -1,8 +1,12 @@
 #ifndef EXPLORERPAGE_H
 #define EXPLORERPAGE_H
 
+#include <QList>
+#include <QListWidgetItem>
 #include <QWidget>
 #include <QStandardItemModel>
+#include <QString>
+#include <QTreeView>
 
 #include "mqtt/message.h"
 
@@ -10,21 +14,49 @@ namespace Ui {
 class ExplorerPage;
 }
 
+class MessageTreeView : public QTreeView
+{
+    Q_OBJECT
+
+public:
+    explicit MessageTreeView(QWidget *parent = nullptr) : QTreeView(parent) {};
+
+signals:
+    void onCurrentChanged(const QModelIndex &current);
+
+private:
+    virtual void currentChanged(const QModelIndex &current, const QModelIndex &previous);
+};
+
 class ExplorerPage : public QWidget
 {
     Q_OBJECT
 
 public:
-    explicit ExplorerPage(QWidget *parent = nullptr);
+    explicit ExplorerPage(QWidget *parent = nullptr, int cap = 50);
     ~ExplorerPage();
 
+    QString current_topic; //<currently selected topic
+
+    void setMessageCap(int cap);
+
+signals:
+    void onChangeSelectedMessage(const int currentRow);
+    void onChangeSelectedTopic(QString topic);
+
 public slots:
+    void changeSelectedMessage(QListWidgetItem *current, QListWidgetItem *previous);
+    void changeSelectedTopic(const QModelIndex &current);
     void initConnection(const QString server_name);
     void receiveNewMessages(const QHash<QString, QList<QString>> new_msgs);
+    void setMessage(QString msg);
+    void setTopic(QList<QString>);
 
 private:
     Ui::ExplorerPage *ui;
     QStandardItemModel topics_tree_model;
+
+    int message_capacity;
 
     void addTopic(const QString topic, const bool root);
 };
