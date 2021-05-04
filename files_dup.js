e63ce858d11b2c9456ab12a5bf93e9e91097ef64
xgross11 <xgross11@users.noreply.github.com>
2021-05-04 16:40:19 +0000

    deploy: 51ff9564ea11e0489a9d35ac95be687023cf5014

--- files_dup.js
+++ files_dup.js
@@ -1,9 +1,16 @@
 var files_dup =
 [
+    [ "dashboardpage.cpp", "dashboardpage_8cpp_source.html", null ],
     [ "dashboardpage.h", "dashboardpage_8h_source.html", null ],
+    [ "explorerpage.cpp", "explorerpage_8cpp_source.html", null ],
     [ "explorerpage.h", "explorerpage_8h_source.html", null ],
+    [ "main.cpp", "main_8cpp_source.html", null ],
+    [ "mainwindow.cpp", "mainwindow_8cpp_source.html", null ],
     [ "mainwindow.h", "mainwindow_8h_source.html", null ],
+    [ "messagestore.cpp", "messagestore_8cpp_source.html", null ],
     [ "messagestore.h", "messagestore_8h_source.html", null ],
+    [ "mqttmanager.cpp", "mqttmanager_8cpp_source.html", null ],
     [ "mqttmanager.h", "mqttmanager_8h_source.html", null ],
+    [ "newconnection.cpp", "newconnection_8cpp_source.html", null ],
     [ "newconnection.h", "newconnection_8h_source.html", null ]
 ];
