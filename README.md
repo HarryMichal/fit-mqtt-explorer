8975cee11f554e59c1983efebe479b24572f600f
Ondřej Míchal <harrymichal@seznam.cz>
2021-05-11 09:57:42 +0200

    Add simulator
    A simulator of traffic is one of the requirements for the assignment.
    
    This adds a new page/stack to the main window. The window has two
    actions related to it: load configuration, toggle simulation.
    
    A simulation is very basic and only supports sending payloads in a set
    interval.
    
    The configuration is done via a JSON file with a specific format. More
    information can be found in the project's README file.

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # fit-mqtt-explorer
+
+## Simulation of traffic
+
+The application has a mode for simulating traffic. The mode can be switched to
+by clicking an appropriate icon (with tooltip "Simulator") in the application's
+toolbar.
+
+Before a simulation can be started, it has to be configured first. A 
+configuration file is a JSON file with specific data defined in it. All data
+has to be correct. If it is not, the simulator will simply not load it.
+
+### Simulation config file structure
+
+The root element is an object with two keys:
+
+- `topic` <`string`> - the root topic where all messages will be sent
+- `devices` <`array`> - list of simulated devices
+
+The `devices` array can hold various number of simulated devices.
+
+Each device is an object with four keys:
+
+- `topic` <`string`> - subtopic where this devices sends messages
+- `type` <`string`> - type of the device
+- `period` <`int`> - timeout between sending messages [ms]
+- `payloads` <`array`> - list of messages to be sent
+
+The `payloads` array can hold various number of payloads. Only paylods of type
+<`string`> have been tested but other types should also be supported.
+
+Example configuration files can be found in [`data/simulations`](https://github.com/HarryMichal/fit-mqtt-explorer/tree/main/data/simulations).
---
+++ src/simulatorpage.cpp
@@ -0,0 +1,178 @@
+#include <QFile>
+#include <QJsonArray>
+#include <QJsonObject>
+#include <QJsonValue>
+
+#include "simulatorpage.h"
+#include "ui_simulatorpage.h"
+
+MockDevice::MockDevice(QString topic, QString type, int period, QList<QVariant> payloads, QObject *parent) :
+    QObject(parent),
+    topic(topic),
+    type(type),
+    period(period),
+    payloads(payloads),
+    payload_iterator(QMutableListIterator<QVariant>(payloads))
+{
+    this->heartbeat = new QTimer(this);
+    connect(this->heartbeat, &QTimer::timeout, this, &MockDevice::handleHeartbeat);
+
+    this->payload_iterator = QMutableListIterator<QVariant>(this->payloads);
+}
+
+MockDevice::~MockDevice()
+{
+    delete this->heartbeat;
+}
+
+void MockDevice::start()
+{
+    this->heartbeat->start(this->period);
+}
+
+void MockDevice::stop()
+{
+    this->heartbeat->stop();
+}
+
+void MockDevice::handleHeartbeat()
+{
+    if (!payload_iterator.hasNext()) {
+        this->payload_iterator.toFront();
+    }
+    this->payload_iterator.next();
+
+    mqtt::string topic = this->topic.toStdString();
+    mqtt::string payload = this->payload_iterator.value().toString().toStdString();
+
+    emit this->onHeartbeat(topic, payload);
+}
+
+SimulatorPage::SimulatorPage(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::SimulatorPage),
+    device_pool(new QList<MockDevice*>)
+{
+    ui->setupUi(this);
+}
+
+SimulatorPage::~SimulatorPage()
+{
+    delete this->ui;
+    delete this->device_pool;
+}
+
+void SimulatorPage::setSimulationConfigFile(QString filename)
+{
+    QFile config_file = QFile(filename);
+
+    if (!config_file.exists()) {
+        return;
+    }
+
+    if (!config_file.open(QFile::ReadOnly)) {
+        return;
+    }
+
+    auto data = config_file.readAll();
+
+    this->config = QJsonDocument::fromJson(data);
+    if (this->config.isNull()) {
+        return;
+    }
+
+    this->configureSimulation();
+}
+
+void SimulatorPage::startSimulation()
+{
+    for (auto it = this->device_pool->begin(); it != this->device_pool->end(); it++) {
+        (*it)->start();
+    }
+}
+
+void SimulatorPage::stopSimulation()
+{
+    for (auto it = this->device_pool->begin(); it != this->device_pool->end(); it++) {
+        (*it)->stop();
+    }
+}
+
+void SimulatorPage::configureSimulation()
+{
+    QJsonValue value;
+
+    // Clean the previous configuration
+    this->stopSimulation();
+    this->device_pool->clear();
+
+    if (!this->config.isObject()) {
+        return;
+    }
+
+    auto root = this->config.object();
+
+    // The root object of the config file should have only 2 keys:
+    //   - base topic name (string)
+    //   - devices (array)
+    if (root.length() != 2) {
+        return;
+    }
+
+    value = root.value("topic");
+    if (value.isUndefined() || !value.isString() ) {
+        return;
+    }
+    this->base_topic = value.toString();
+
+    value = root.value("devices");
+    if (value.isUndefined() || !value.isArray()) {
+        return;
+    }
+    auto device_list = value.toArray();
+
+    // Iterate over all defined devices in the config and convert them into
+    // MockDevice.
+    for (auto it = device_list.begin(); it != device_list.end(); it++) {
+        QJsonObject device;
+        QString topic, type;
+        int period;
+
+        // Every device in the array has to be an object
+        if (!it->isObject()) {
+            return;
+        }
+
+        device = it->toObject();
+       
+        value = device.value("topic");
+        if (value.isUndefined() || !value.isString()) {
+            return;
+        }
+        topic = base_topic + "/" + value.toString();
+
+        value = device.value("type");
+        if (value.isUndefined() || !value.isString()) {
+            return;
+        }
+        type = value.toString();
+
+        value = device.value("period");
+        if (value.isUndefined() || !value.isDouble()) {
+            return;
+        }
+        period = value.toInt();
+
+        value = device.value("payloads");
+        if (value.isUndefined() || !value.isArray()) {
+            return;
+        }
+        QList<QVariant> payloads = value.toArray().toVariantList();
+
+        MockDevice *new_mock_device = new MockDevice(topic, type, period, payloads);
+        connect(new_mock_device, &MockDevice::onHeartbeat, this, &SimulatorPage::onHeartbeat);
+        this->device_pool->append(new_mock_device);
+    }
+
+    emit this->onReady();
+}
